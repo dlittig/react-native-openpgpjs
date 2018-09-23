a2d8d5901c16e9f36d968ef8288121baf59b5100
David Littig <david0605.littig@hotmail.de>
2018-09-23 19:24:05 +0200

    Added `react-native-crypto` for crypto polyfill

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import { shim } from './shim'
 
 const { RNOpenpgpjs } = NativeModules
 
+console.log(window)
+
+if(window !== null) {
+  console.log(window.crypto)
+}
+
 shim()
 const openpgp = require('./node_modules/openpgp/dist/openpgp.min')
 export default { ...openpgp }
