c82f5316a91cc0308478c9cc7a39b6ea9ee89bd7
David Littig <david0605.littig@hotmail.de>
2018-09-23 19:52:02 +0200

    reverted

--- index.js
+++ index.js
@@ -3,12 +3,5 @@ import { shim } from './shim'
 
 const { RNOpenpgpjs } = NativeModules
 
-console.log(window)
-
-if(window !== null) {
-  console.log(window.crypto)
-}
-
 shim()
-const openpgp = require('./node_modules/openpgp/dist/openpgp.min')
 export default { ...openpgp }
